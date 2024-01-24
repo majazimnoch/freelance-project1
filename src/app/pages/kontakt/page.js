efcaecd93efee3fdfc3fb3b77d157376e2f10e6f
majazimnoch <majamzimnoch@gmail.com>
2024-01-24 17:32:36 +0100

    embedded a google map

--- src/app/pages/kontakt/page.js
+++ src/app/pages/kontakt/page.js
@@ -1,3 +1,4 @@
+import Kontakt from '@/app/components/Kontakt';
 import Navbar from '@/app/components/Navbar'
 import Link from 'next/link';
 import React from 'react'
@@ -6,6 +7,7 @@ export default function page() {
   return (
     <div>
       <Navbar />
+      <Kontakt />
     </div>
   )
 }
