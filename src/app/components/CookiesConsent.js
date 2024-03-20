ff0ed3fc2d592769408a7d411ccc4fdc3c7673d6
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2024-03-20 21:56:29 +0100

    added a new page for cookies content to read

--- src/app/components/CookiesConsent.js
+++ src/app/components/CookiesConsent.js
@@ -23,11 +23,11 @@ export default function CookiesConsent() {
 
   return (
     <div className='fixed inset-0 bg-white bg-opacity-70 z-[5000]'>
-      <div className='fixed bottom-0 left-0 flex items-center justify-between px-4 py-8 bg-primary'>
+      <div className='fixed bottom-0 left-0 flex items-center justify-between gap- px-4 py-8 bg-primary'>
         <span className=''>We use cookies to enhance your browsing experience. By clicking "Accept," you consent to our cookie usage. Read more about it <Link href="/"> here </Link></span>
         <Button 
             buttonText="Accept" 
-            link="/pages/kontakt"  
+            link="/pages/oferta"  
             onClick={() => {
             console.log("Button clicked!");
             }}/>
--- src/app/pages/oferta/page.js
+++ src/app/pages/oferta/page.js
@@ -1,9 +1,16 @@
+import Footer from '@/app/components/Footer'
+import Kontakt from '@/app/components/Kontakt'
+import Navbar from '@/app/components/Navbar'
 import React from 'react'
 
 export default function Latest() {
   return (
+    <div className="bg-background h-screen flex flex-col justify-between">
     <div>
-      dwqdasdsd
+      <Navbar />
+      cookies content
     </div>
+    <Footer />
+  </div>
   )
 }
