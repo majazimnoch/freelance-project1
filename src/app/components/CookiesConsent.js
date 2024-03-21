059bc3176ebe494b9513da57d6d48899900a5976
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2024-03-21 20:13:03 +0100

    functional cookies, wohoo

--- src/app/components/CookiesConsent.js
+++ src/app/components/CookiesConsent.js
@@ -1,34 +1,37 @@
 "use client"
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { hasCookie, setCookie } from 'cookies-next'
 import Link from 'next/link'
 import Button from './Button';
 
 export default function CookiesConsent() {
+  const [showConsent, setShowConsent] = useState(false);
 
-  const [showConsent, setShowConsent] = React.useState( true );
+  useEffect(() => {
+    if (!hasCookie('localConsent')) {
+        setShowConsent(true);
+    }
+  }, []);
 
-  React.useEffect( () => {
-    setShowConsent(hasCookie("localConsent"));
-  },[])
 
   const acceptCookie = () => {
-    setShowConsent(true);
+    setShowConsent(false);
     setCookie("localConsent", "true", {})
   };
 
   if (showConsent) {
+    return (
+      <div className="fixed inset-0 bg-white bg-opacity-70 z-[5000]">
+        <div className="fixed bottom-0 left-0 flex items-center justify-between gap-4 px-4 py-8 bg-primary">
+          <span className="">
+            We use cookies to enhance your browsing experience. By clicking "Accept," you consent to our cookie usage. Read more about it{' '}
+            <Link href="/"> here </Link>
+          </span>
+          <Button buttonText="Accept" onClick={acceptCookie} />
+        </div>
+      </div>
+    );
+  } else {
     return null;
   }
-
-  return (
-    <div className='fixed inset-0 bg-white bg-opacity-70 z-[5000]'>
-      <div className='fixed bottom-0 left-0 flex items-center justify-between gap- px-4 py-8 bg-primary'>
-        <span className=''>We use cookies to enhance your browsing experience. By clicking "Accept," you consent to our cookie usage. Read more about it <Link href="/"> here </Link></span>
-        <Button buttonText="Accept" 
-            link="/"  
-            onClick={() => acceptCookie()} />
-      </div>
-    </div>
-  )
 }
