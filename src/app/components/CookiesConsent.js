902ed08dfafedfc69022636cb57b17a404111d95
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2024-03-22 22:47:22 +0100

    added a year expiration to cookies

--- src/app/components/CookiesConsent.js
+++ src/app/components/CookiesConsent.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 import Button from "./Button";
 
 export default function CookiesConsent() {
-  const [showConsent, setShowConsent] = useState(true);
+  const [showConsent, setShowConsent] = useState(false);
 
   useEffect(() => {
     if (!hasCookie("localConsent")) {
@@ -15,7 +15,10 @@ export default function CookiesConsent() {
 
   const acceptCookie = () => {
     setShowConsent(false);
-    setCookie("localConsent", "true", {});
+
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 365); // Set expiration to 1 year from now
+    setCookie("localConsent", "true", { expires: expirationDate });
   };
 
   if (showConsent) {
@@ -37,3 +40,48 @@ export default function CookiesConsent() {
     return null;
   }
 }
+ 
+/* 
+"use client";
+import React, { useState, useEffect } from "react";
+import { hasCookie, setCookie } from "cookies-next";
+import Link from "next/link";
+import Button from "./Button";
+
+export default function CookiesConsent() {
+  const [showConsent, setShowConsent] = useState(true);
+
+  useEffect(() => {
+    if (!hasCookie("localConsent")) {
+      setShowConsent(false);
+    }
+  }, []);
+
+  const acceptCookie = () => {
+    setShowConsent(true);
+    setCookie("localConsent", "true", {});
+  };
+
+  if (showConsent) {
+    return null;
+  }
+
+  if (showConsent) {
+    return (
+      <div className="fixed inset-0 bg-white bg-opacity-70 z-[5000] flex justify-center items-end">
+        <div className="w-full">
+          <div className="bg-primary px-4 py-8 flex items-center justify-between gap-4">
+            <span className="">
+              We use cookies to enhance your browsing experience. By clicking
+              "Accept," you consent to our cookie usage. Read more about it{" "}
+              <Link href="/pages/oferta"> here </Link>
+            </span>
+            <Button buttonText="Accept" onClick={acceptCookie} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+} */
