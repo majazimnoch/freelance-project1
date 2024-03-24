b532ee54eebc82270069c61ca4d2397f4ad787b7
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2024-03-24 22:38:30 +0100

    delay broke smth in the code and fixed it but without delay

--- src/app/components/CookiesConsent.js
+++ src/app/components/CookiesConsent.js
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import { hasCookie, setCookie } from "cookies-next";
 import Link from "next/link";
 import Button from "./Button";
 
@@ -7,21 +8,22 @@ export default function CookiesConsent() {
   const [showConsent, setShowConsent] = useState(false);
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
+    if (!hasCookie("localConsent")) {
       setShowConsent(true);
-    }, 3000); 
-
-    // Clean up the timeout to prevent memory leaks
-    return () => clearTimeout(timeout);
-  }, []); // Run once on component mount
+    }
+  }, []);
 
   const acceptCookie = () => {
     setShowConsent(false);
+
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 365); // Set expiration to 1 year from now
+    setCookie("localConsent", "true", { expires: expirationDate });
   };
 
   if (showConsent) {
     return (
-      <div className="fixed inset-0 bg-white bg-opacity-70 z-[5000] flex justify-center items-end">
+        <div className={`fixed bottom-0 left-0 w-full bg-white flex justify-center items-end transition-opacity duration-500 ease-in-out ${showConsent ? 'opacity-100' : 'opacity-0'}`}>
         <div className="w-full">
           <div className="bg-primary px-4 py-8 flex items-center justify-between gap-4">
             <span className="">
@@ -38,50 +40,3 @@ export default function CookiesConsent() {
     return null;
   }
 }
-
-
- 
-/* 
-"use client";
-import React, { useState, useEffect } from "react";
-import { hasCookie, setCookie } from "cookies-next";
-import Link from "next/link";
-import Button from "./Button";
-
-export default function CookiesConsent() {
-  const [showConsent, setShowConsent] = useState(true);
-
-  useEffect(() => {
-    if (!hasCookie("localConsent")) {
-      setShowConsent(false);
-    }
-  }, []);
-
-  const acceptCookie = () => {
-    setShowConsent(true);
-    setCookie("localConsent", "true", {});
-  };
-
-  if (showConsent) {
-    return null;
-  }
-
-  if (showConsent) {
-    return (
-      <div className="fixed inset-0 bg-white bg-opacity-70 z-[5000] flex justify-center items-end">
-        <div className="w-full">
-          <div className="bg-primary px-4 py-8 flex items-center justify-between gap-4">
-            <span className="">
-              We use cookies to enhance your browsing experience. By clicking
-              "Accept," you consent to our cookie usage. Read more about it{" "}
-              <Link href="/pages/oferta"> here </Link>
-            </span>
-            <Button buttonText="Accept" onClick={acceptCookie} />
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    return null;
-  }
-} */
