e30e11f91aaafc6e69acd93b72e59bf3f52f86ed
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2024-03-24 22:44:48 +0100

    added setShowOwerlay to prevent from clicking when cookies not accepted

--- src/app/components/CookiesConsent.js
+++ src/app/components/CookiesConsent.js
@@ -6,15 +6,18 @@ import Button from "./Button";
 
 export default function CookiesConsent() {
   const [showConsent, setShowConsent] = useState(false);
+  const [showOverlay, setShowOverlay] = useState(false);
 
   useEffect(() => {
     if (!hasCookie("localConsent")) {
       setShowConsent(true);
+      setShowOverlay(true); // Show overlay when consent is not given
     }
   }, []);
 
   const acceptCookie = () => {
     setShowConsent(false);
+    setShowOverlay(false); // Hide overlay after accepting cookies
 
     const expirationDate = new Date();
     expirationDate.setDate(expirationDate.getDate() + 365); // Set expiration to 1 year from now
@@ -22,15 +25,18 @@ export default function CookiesConsent() {
   };
 
   return (
-    <div className={`fixed bottom-0 left-0 w-full bg-white flex justify-center items-end transition-opacity duration-500 ease-in-out ${showConsent ? 'opacity-100' : 'opacity-0'}`}>
-      <div className="w-full">
-        <div className="bg-primary px-4 py-8 flex items-center justify-between gap-4">
-          <span className="text-background">
-            We use cookies to enhance your browsing experience. By clicking
-            "Accept," you consent to our cookie usage. Read more about it{" "}
-            <Link href="/pages/oferta"> here </Link>
-          </span>
-          <Button buttonText="Accept" onClick={acceptCookie} />
+    <div>
+      <div className={`fixed top-0 left-0 w-full h-full bg-black opacity-50 transition-opacity duration-500 ${showOverlay ? 'block' : 'hidden'}`}></div>
+      <div className={`fixed bottom-0 left-0 w-full bg-white flex justify-center items-end transition-opacity duration-500 ease-in-out ${showConsent ? 'opacity-100' : 'opacity-0'}`}>
+        <div className="w-full">
+          <div className="bg-primary px-4 py-8 flex items-center justify-between gap-4">
+            <span className="text-background">
+              We use cookies to enhance your browsing experience. By clicking
+              "Accept," you consent to our cookie usage. Read more about it{" "}
+              <Link href="/pages/oferta"> here </Link>
+            </span>
+            <Button buttonText="Accept" onClick={acceptCookie} />
+          </div>
         </div>
       </div>
     </div>
