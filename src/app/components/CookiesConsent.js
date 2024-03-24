dcfb121ce3414e298e1b881b1dcb86f9bede3f67
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2024-03-24 22:41:09 +0100

    added opacity

--- src/app/components/CookiesConsent.js
+++ src/app/components/CookiesConsent.js
@@ -21,22 +21,18 @@ export default function CookiesConsent() {
     setCookie("localConsent", "true", { expires: expirationDate });
   };
 
-  if (showConsent) {
-    return (
-        <div className={`fixed bottom-0 left-0 w-full bg-white flex justify-center items-end transition-opacity duration-500 ease-in-out ${showConsent ? 'opacity-100' : 'opacity-0'}`}>
-        <div className="w-full">
-          <div className="bg-primary px-4 py-8 flex items-center justify-between gap-4">
-            <span className="">
-              We use cookies to enhance your browsing experience. By clicking
-              "Accept," you consent to our cookie usage. Read more about it{" "}
-              <Link href="/pages/oferta"> here </Link>
-            </span>
-            <Button buttonText="Accept" onClick={acceptCookie} />
-          </div>
+  return (
+    <div className={`fixed bottom-0 left-0 w-full bg-white flex justify-center items-end transition-opacity duration-500 ease-in-out ${showConsent ? 'opacity-100' : 'opacity-0'}`}>
+      <div className="w-full">
+        <div className="bg-primary px-4 py-8 flex items-center justify-between gap-4">
+          <span className="text-background">
+            We use cookies to enhance your browsing experience. By clicking
+            "Accept," you consent to our cookie usage. Read more about it{" "}
+            <Link href="/pages/oferta"> here </Link>
+          </span>
+          <Button buttonText="Accept" onClick={acceptCookie} />
         </div>
       </div>
-    );
-  } else {
-    return null;
-  }
+    </div>
+  );
 }
